3fd7f24082cc16c85320aac3f0ffe136878a1410
Åsmund Norderud <aasmund.norderud@gmail.com>
2022-07-17 04:56:41 +0000

    test

---
+++ src/components/InputChar.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { TypingChar, CharState } from '@/classes/TypingClasses'
+export default {
+  props: {
+    inputChar: {
+      char: TypingChar,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      state: CharState.Default,
+    }
+  },
+}
+</script>
+
+<template>
+  <span> {{ type.char }}</span>
+</template>
